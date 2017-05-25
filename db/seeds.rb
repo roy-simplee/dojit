da6a1930c8f3ec461a0dbf8a93876774602fbedf
Saba <roy@simplee.com>
2017-05-25 15:48:23 +0300

    Install Faker gem, seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+# Create posts
+50.times do
+	Post.create!(
+		title: Faker::Lorem.sentence,
+		body: Faker::Lorem.paragraph)
+end
+
+posts = Post.all
+
+# Create comments
+100.times do
+	Comment.create!(
+		post: posts.sample,
+		body: Faker::Lorem.paragraph)
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
