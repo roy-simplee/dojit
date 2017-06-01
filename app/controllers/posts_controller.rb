1aab1f50d43f9f3fbefd21334659865b9e37ccf5
Saba <roy@simplee.com>
2017-06-01 17:19:07 +0300

    refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(user_params)
     @post.topic = @topic
     authorize @post
     if @post.save
@@ -34,12 +34,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @topic = @post.topic
     authorize @post
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(user_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
     end
+  end
+
+  private def user_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -21,7 +21,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    @topic = Topic.new(user_params)
     authorize @topic
     if @topic.save
       redirect_to @topic, notice: "Topic was saved."
@@ -34,12 +34,16 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
     authorize @topic
-    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+    if @topic.update_attributes(user_params)
       flash[:notice] = "Topic was updated."
       redirect_to @topic
     else
       flash[:error] = "Error saving topic. Please try again."
       render :edit
     end
+  end
+
+  private def user_params
+    params.require(:topic).permit(:name, :description, :public)
   end
 end
