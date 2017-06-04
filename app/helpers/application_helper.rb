d46406a2ee7fbc1785ce7bf2a18eaaf9bbf83561
Saba <roy@simplee.com>
2017-06-04 11:57:01 +0300

    markdown impl

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,16 @@ module ApplicationHelper
   def form_group_tag(errors)
     result = 'form-group'
     if errors.any?
-      result.' has-error'
+      result << ' has-error'
     else
       ''
     end
+  end
+
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
   end
 end
