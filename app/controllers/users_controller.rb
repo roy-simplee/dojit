a0a632ab8ff148790481138758248c9a8877013a
Saba <roy@simplee.com>
2017-06-04 17:12:15 +0300

    avatar code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
   end
 
   private def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :avatar)
   end
 end
