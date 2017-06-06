9e244fa6e39e9392c3696ed3a64fd92c987aa2be
Saba <roy@simplee.com>
2017-06-06 17:59:40 +0300

    temp

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -4,17 +4,15 @@ describe Post do
   describe "vote methods" do
 
     before do
-      user = User.create
-      topic = Topic.create
-      @post = Post.create(title: 'post title', body: 'post b                         ody', user: user, topic: topic)
-      3.times { @post.votes.create(value: 1, user: user, post: @post) }
-      2.times { @post.votes.create(value: -1, user: user, post: @post) }
-    end
-
-    describe '#up_votes' do
-      it "counts the number of votes with value = 1" do
-        expect( @post.up_votes ).to eq(3)
-      end
+      # user = User.create
+      # topic = Topic.create
+      @post = Post.new(title: 'post title', body: 'post b                         ody')
+      post_user = double(votes: @post.votes)
+      allow(@post).to receive(:user) {post_user}
+      p @post.to_json
+      @post.save
+      3.times { @post.votes.create(value: 1) }
+      2.times { @post.votes.create(value: -1) }
     end
 
     describe '#down_votes' do
