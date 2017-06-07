fac87d6133c2aea136f72cb630a689c73bbe736b
Saba <roy@simplee.com>
2017-06-07 13:06:41 +0300

    n

--- spec/model/vote_spec.rb
+++ spec/model/vote_spec.rb
@@ -4,12 +4,12 @@ describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-      user = User.create
-      topic = Topic.create
-      post = Post.create(title: 'post title', body: 'post b                         ody', user: user, topic: topic)
-        expect(Vote.create(:value => 1, user: user, post: post).valid?).to eq(true)
-        expect(Vote.create(value: -1, user: user, post: post).valid?).to eq(true)
-        expect(Vote.create(value: 2, user: user, post: post).valid?).to eq(false)
+        user = User.create
+        topic = Topic.create
+        post = Post.create(title: 'post title', body: 'post b                         ody', user: user, topic: topic)
+          expect(Vote.create(:value => 1, user: user, post: post).valid?).to eq(true)
+          expect(Vote.create(value: -1, user: user, post: post).valid?).to eq(true)
+          expect(Vote.create(value: 2, user: user, post: post).valid?).to eq(false)
       end
     end
   end
