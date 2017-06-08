e668310f9b9b7bab26033c6279eac44f05817271
Saba <roy@simplee.com>
2017-06-08 13:29:20 +0300

    done

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 gem 'faker'
 gem 'bootstrap-sass'
+gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
 group :production do
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
+
   def new
     @post = Post.find(params[:post_id])
     @comment = Comment.new
@@ -19,25 +21,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # def edit
-  #   @post = Post.find(params[:id])
-  #   @topic = @post.topic
-  #   authorize @post
-  # end
-
-  # def update
-  #   @post = Post.find(params[:id])
-  #   @topic = @post.topic
-  #   authorize @post
-  #   if @post.update_attributes(user_params)
-  #     flash[:notice] = "Post was updated."
-  #     redirect_to [@topic, @post]
-  #   else
-  #     flash[:error] = "There was an error saving the post. Please try again."
-  #     render :edit
-  #   end
-  # end
-
   def destroy
     @comment = Comment.find(params[:id])
     post = @comment.post
@@ -45,10 +28,12 @@ class CommentsController < ApplicationController
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment deleted."
-      redirect_to [topic, post]
     else
       flash[:error] = "Error deleting comment. Please try again."
-      render :show
+    end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [topic, @post] }
     end
   end
 
